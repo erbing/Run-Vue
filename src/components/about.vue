ad5eda0659277336bdb025821a4889dafbdb661b
zigzga <447587073@qq.com>
2016-12-17 23:44:51 +0800

    add someone

--- src/components/about.vue
+++ src/components/about.vue
@@ -10,13 +10,13 @@
 
                 <tr>
                     <td><h4>项目管理</h4></td>
-                    <td>Zigzag</td>
+                    <td><a href="https://github.com/erbing" target="_blank">@Zigzag</a></td>
                     <td>发起、协调、杂务</td>
                 </tr>
 
                 <tr>
                     <td><h4>组件开发</h4></td>
-                    <td>Run —— UED</td>
+                    <td> <a>@秋水</a> </td>
                     <td>组件的开发以及维护</td>
                 </tr>
 
@@ -37,13 +37,16 @@
     }
     .table{
         border: 1px solid #ddd;
+        border-spacing: 0;
+        border-collapse: collapse;
         tr{
             padding:0;
             margin: 0;
             td {
                 padding:0;
                 margin: 0;
-                border: 1px solid #ddd;
+                border-right: 1px solid #ddd;
+                border-bottom: 1px solid #ddd;
                 text-align: center;
             }    
         }
