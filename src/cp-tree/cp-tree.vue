4cac60070c194e3596537a614ddaf1739ff8679d
zigzag <447587073@qq.com>
2016-12-22 10:25:40 +0800

    add tree cpt

---
+++ src/cp-tree/cp-tree.vue
@@ -0,0 +1,75 @@
+<template>
+    <!--<div id="tree">123</div>-->
+    <div id="cp-tree">
+        <li>
+            <div
+            :class="{bold: isFolder}"
+            @click="toggle"
+            @dblclick="changeType">
+            {{model.name}}
+            <span v-if="isFolder">[{{open ? '-' : '+'}}]</span>
+            </div>
+            <ul v-show="open" v-if="isFolder">
+            <cp-tree
+                class="item"
+                v-for="model in model.children"
+                :model="model">
+            </cp-tree>
+            <li class="add" @click="addChild">+</li>
+            </ul>
+        </li>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: 'cp-tree',
+        props: {
+            model: Object
+        },
+        data () {
+            return {
+                open: false
+            }
+        },
+        computed: {
+            isFolder () {
+                return this.model.children && this.model.children.length
+            }
+        },
+        methods: {
+            toggle () {
+                if (this.isFolder) {
+                    this.open = !this.open
+                }
+            },
+            changeType () {
+                if (!this.isFolder) {
+                    this.$Vue.set(this.model, 'children', [])
+                    this.addChild()
+                    this.open = true
+                }
+            },
+            addChild () {
+                this.model.children.push({
+                    name: 'new stuff'
+                })
+            }
+        }
+    }
+</script>
+
+<style lang="less">
+.item {
+  cursor: pointer;
+}
+.bold {
+  font-weight: bold;
+}
+ul {
+  padding-left: 1em;
+  line-height: 1.5em;
+  list-style-type: dot;
+}
+</style>
--- src/tree/tree.vue
+++ src/tree/tree.vue
@@ -1,73 +1,54 @@
 <template>
-    <div id="tree">123</div>
-    <!--<script type="text/x-template" id="item-template">
-    <li>
-        <div
-        :class="{bold: isFolder}"
-        @click="toggle"
-        @dblclick="changeType">
-        {{model.name}}
-        <span v-if="isFolder">[{{open ? '-' : '+'}}]</span>
-        </div>
-        <ul v-show="open" v-if="isFolder">
-        <item
-            class="item"
-            v-for="model in model.children"
-            :model="model">
-        </item>
-        <li class="add" @click="addChild">+</li>
+    <div id="tree">
+        <ul>
+            <item
+                class="item"
+                :model="treeData">
+            </item>
         </ul>
-    </li>
-    </script>-->
+    </div>
 </template>
 
 <script>
+    import cpTree from '../cp-tree/cp-tree'
     export default {
-        props: {
-            model: Object
+        name: 'tree',
+        components: {
+            'item': cpTree
         },
         data () {
             return {
-                open: false
-            }
-        },
-        computed: {
-            isFolder () {
-                return this.model.children && this.model.children.length
-            }
-        },
-        methods: {
-            toggle () {
-                if (this.isFolder) {
-                    this.open = !this.open
+                treeData: {
+                    name: 'My Tree',
+                    children: [
+                        { name: 'hello' },
+                        { name: 'wat' },
+                        {
+                            name: 'child folder',
+                            children: [
+                                {
+                                    name: 'child folder',
+                                    children: [
+                                        { name: 'hello' },
+                                        { name: 'wat' }
+                                    ]
+                                },
+                                { name: 'hello' },
+                                { name: 'wat' },
+                                {
+                                    name: 'child folder',
+                                    children: [
+                                        { name: 'hello' },
+                                        { name: 'wat' }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
                 }
-            },
-            changeType () {
-                if (!this.isFolder) {
-                    this.$Vue.set(this.model, 'children', [])
-                    this.addChild()
-                    this.open = true
-                }
-            },
-            addChild () {
-                this.model.children.push({
-                    name: 'new stuff'
-                })
             }
         }
     }
 </script>
 
-<style lang="less">
-.item {
-  cursor: pointer;
-}
-.bold {
-  font-weight: bold;
-}
-ul {
-  padding-left: 1em;
-  line-height: 1.5em;
-  list-style-type: dot;
-}
-</style>
+<style lang="less"></style>
