16c7168b92ba5cc609eb72cfd084c9f8edfafc0e
zigzga <447587073@qq.com>
2016-12-23 23:58:19 +0800

    change slide

--- src/slide/slide.vue
+++ src/slide/slide.vue
@@ -40,6 +40,7 @@
         <cp-select :title=" '关于我们' " :to=" '/about' " :plus="true"></cp-select>
         <hr class="divider">
         <cp-list :texts=" aboutUsText "></cp-list>
+        <cp-list :texts=" aboutUsText "></cp-list>
     </div>
 
 </template>
