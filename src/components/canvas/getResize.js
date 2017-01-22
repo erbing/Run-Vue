89097b552147226a80ecc8285c8c9b5bf079b9ac
zigzag <447587073@qq.com>
2017-01-22 09:31:34 +0800

    222

--- src/components/canvas/getResize.js
+++ src/components/canvas/getResize.js
@@ -1,16 +1,10 @@
 let screenWidth = document.body.clientWidth - 256
 
-const resizeFunc = function () {
-    window.onresize = function () {
-        screenWidth = document.body.clientWidth - 256
-        return screenWidth
-    }
+window.onresize = function () {
+    screenWidth = document.body.clientWidth - 256
+    return screenWidth
 }
 
-<<<<<<< HEAD
 const onresize = window.onresize()
-=======
-export default {resizeFunc}
->>>>>>> d4a9bab5492a80913eed7312ae1864491db51988
 
 export default onresize
