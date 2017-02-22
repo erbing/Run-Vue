d7977ccd16edd7ed5de2881111ad54211b3692ab
温竞标 <wenjingbiao@panyou.com>
2017-02-22 19:36:07 +0800

    添加ticker説明文檔，刪除組件注釋

--- src/components/func/ticker.vue
+++ src/components/func/ticker.vue
@@ -1,27 +1,10 @@
 <script>
-    /**
-     *  author : fitz/秋水
-     *  email  : wenjingbiao@outlook.com
-     * @props     ticks               Number            倒計時秒數      如：60
-     * @props     auto                Boolean           自动倒计时，    默认为true,
-     * @methods   tick()                                啓動倒計時
-     * @methods   stop()                                清除倒計時
-     * @v-model                                         在組件使用時綁定一個v-model 可以獲取倒計時的秒數，觀察這個v-model 可以做其他事情
-     *
-     * 例子
-     *     1、注冊 ticker 組件
-     *     3、調用 tick()
-     *         this.$refs.ticker.tick()
-     *     4、調用stop()
-     *         this.$refs.ticker.stop()
-     * **/
     export default{
         name: 'tick',
         render(createElement) {
             return createElement('span',{},this.$slots.default)
         },
         props: {
-
             ticks: {
                 type: Number,
                 default: 10
@@ -31,13 +14,6 @@
                 default: true
             }
         },
-        data(){
-            return {
-                currentValue: 0,
-                text: this.label
-            }
-        },
-
         mounted(){
             this.$nextTick(()=> {
                 if (this.auto === true) {
@@ -48,7 +24,7 @@
         methods: {
             stop(){
                 clearInterval(this.ticker);
-                this.$emit('input', this.currentValue = 0)
+                this.$emit('input',  0)
             },
             tick(){
                 let val = this.ticks;
@@ -59,7 +35,6 @@
                     } else {
                         val--;
                     }
-                    this.currentValue = val;
                     this.$emit('input', val);
                 }, 1000);
             }
