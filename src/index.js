a25ab7dcf44816141dbafdb97bd0ae7b928604c5
zigzag <447587073@qq.com>
2017-02-24 09:55:48 +0800

    add index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,50 @@
 // es6 polyfill
-// import 'core-js/fn/array/find-index';
+
+// 基础组件
 import uiIcon from './components/base/icon'
+import uiButton from './components/base/button'
+import uiCol from './components/base/col'
+import uiRow from './components/base/row'
+import uiTable from './components/base/table'
+// 表单组件
+import uiCheckbox from './components/form/checkbox'
+import uiRadio from './components/form/radio'
+import uiInput from './components/form/input'
+import uiInputNumber from './components/form/inputnumber'
+import uiAutoComplete from './components/form/autocomplete'
+import uiSearch from './components/form/search'
+import uiTextarea from './components/form/textarea'
+import uiSwitch from './components/form/switch'
+import uiSelect from './components/form/select'
+// 功能组件
+import uiSwipe from './components/func/swipe'
+import uiPicker from './components/func/picker'
+import uiToast from './components/func/toast'
+import uiModal from './components/func/modal'
+import uiProgress from './components/func/progress'
+import uiBackTop from './components/func/backTop'
 
 const runvue = {
-    uiIcon
+    uiIcon,
+    uiButton,
+    uiCol,
+    uiRow,
+    uiTable,
+    uiCheckbox,
+    uiRadio,
+    uiInput,
+    uiInputNumber,
+    uiAutoComplete,
+    uiSearch,
+    uiTextarea,
+    uiSwitch,
+    uiSelect,
+    uiSwipe,
+    uiPicker,
+    uiToast,
+    uiModal,
+    uiProgress,
+    uiBackTop
 }
 
 const install = function (Vue, opts = {}) {
